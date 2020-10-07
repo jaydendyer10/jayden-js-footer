9f0a5817997888a1e82f6cdf79a5e6f88de70ad2
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-10-07 14:12:46 -0600

    package.json and code for footer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+
+'use strict';
+var moment = require('moment');
+/**
+ * Returns a div element with a footer and updating year
+ * @param {string} name
+ * @return {string}
+ */
+exports.footer = function (name) {
+    return "Copyright " + moment().format('YYYY') + " " + name + " All rights reserved";
+};
